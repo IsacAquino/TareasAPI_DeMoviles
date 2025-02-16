10f462da3783938fa5e8d17ca259eb5bf0edcdec
IsacAquino <ia2021-3210@uce.edu.do>
2025-02-15 23:10:49 -0400

    versiones cambiadas

--- TareasAPI/obj/Debug/net6.0/TareasAPI.AssemblyInfo.cs
+++ TareasAPI/obj/Debug/net6.0/TareasAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b488f057460d13b52427b04e64a14f5ba9baee08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c21df4cdbca1126bff78266ae49c2f33500441")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
